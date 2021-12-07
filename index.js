78dbaf6917735cb96336834a6bacfd048f30f90e
guochao <463561248@qq.com>
2021-12-07 21:13:12 +0800

    ✨ feat: 生成文件的调整

--- index.js
+++ index.js
@@ -1,19 +1,45 @@
 /*
  * @Author: saber
  * @Date: 2021-12-03 15:48:13
- * @LastEditTime: 2021-12-07 18:05:16
+ * @LastEditTime: 2021-12-07 21:12:54
  * @LastEditors: saber
- * @Description: 
+ * @Description:
  */
-const { generateApi } = require('swagger-typescript-api');
+const { generateApi } = require("swagger-typescript-api");
 const path = require("path");
+const { resolve } = require("path");
 const fs = require("fs");
+const makeDir = require("make-dir");
+const pathIsExist = (path) => path && fs.existsSync(path);
+
+const removeDir = (path) => {
+  try {
+    fs.rmSync(path, { recursive: true });
+  } catch (e) {}
+};
+
+const createDir = (path) => {
+  try {
+    makeDir.sync(path);
+  } catch (e) {}
+};
+
+const cleanDir = (path) => {
+  removeDir(path);
+  createDir(path);
+};
+const createFile = ({ path, fileName, content, withPrefix }) =>
+  fs.writeFileSync(
+    resolve(__dirname, path, `./${fileName}`),
+    `${withPrefix ? filePrefix : ""}${content}`,
+    () => {}
+  );
 generateApi({
-  url: 'http://localhost:5000/github-swagger.json',
+  url: "http://localhost:5000/github-swagger.json",
   name: "MySuperbApi.ts",
-  output: path.resolve(process.cwd(), './src/api'),
-  // output: false,
-  templates: path.resolve(process.cwd(), './templates'),
+  // output: path.resolve(process.cwd(), './src/api'),
+  output: false,
+  templates: path.resolve(process.cwd(), "./templates"),
   httpClientType: "axios",
   modular: true,
   generateRouteTypes: false,
@@ -23,15 +49,15 @@ generateApi({
   hooks: {
     onCreateComponent: (component) => {
       // console.log('component----', component)
-      return component 
+      return component;
     },
     onCreateRequestParams: (rawType) => {
       // console.log('rawType------', rawType)
-      return rawType; 
+      return rawType;
     },
     onCreateRoute: (routeData) => {
       // console.log('routeData----', routeData)
-      return routeData 
+      return routeData;
     },
     onCreateRouteName: (routeNameInfo, rawRouteInfo) => {},
     onFormatRouteName: (routeInfo, templateRouteName) => {},
@@ -39,18 +65,40 @@ generateApi({
     onInit: (configuration) => {
       // console.log('configuration', configuration)
       // TODO: 解决一下  tags 中文的问题
-      return configuration; 
+      return configuration;
     },
     onParseSchema: (originalSchema, parsedSchema) => {},
     onPrepareConfig: (currentConfiguration) => {
       // console.log('currentConfiguration', currentConfiguration.modelTypes)
       return currentConfiguration;
-    }
-  }
-}).then(({ files, configuration }) => {
-  // console.log('files', files)
-  // files.forEach(({ content, name }) => {
-  //   fs.writeFile(path, content);
-  // });
+    },
+  },
 })
-.catch(e => console.error(e))
+  .then(({ files, configuration }) => {
+    // console.log('files', files)
+    const output = path.resolve(process.cwd(), './src/api');
+    const cleanOutput = true;
+    
+    if (pathIsExist(output)) {
+      if (cleanOutput) {
+        cleanDir(output);
+      }
+    } else {
+      createDir(output);
+    }
+    files.forEach(({ content, name }) => {
+      if(name === 'http-client.ts'){
+        return;
+      }
+      if(name === 'data-contracts.ts'){
+        name = 'data.d.ts'
+      }
+      createFile({
+        path: path.resolve(process.cwd(), './src/api'),
+        fileName: name,
+        content,
+        withPrefix: false,
+      });
+    });
+  })
+  .catch((e) => console.error(e));
--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,12 +1,12 @@
 /*
  * @Author: saber
  * @Date: 2021-12-06 20:27:48
- * @LastEditTime: 2021-12-07 16:05:00
+ * @LastEditTime: 2021-12-07 21:12:13
  * @LastEditors: saber
  * @Description: 
  */
 import SaberHttp from '@digitforce/saber-http';
-import custom_http from '@digitforce/saber-http/dist/js/modern/custom-http'∏
+import custom_http from '@digitforce/saber-http/dist/js/modern/custom-http';
 export const http = custom_http as SaberHttp;
 // http.request({
 //   url:'',
